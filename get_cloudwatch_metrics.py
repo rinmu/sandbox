3e09eeb787237b3bf5116cd1e5a5822c8aade3be
Tomoyuki Suzuki <tomoyuki@jmty.jp>
2017-01-13 17:08:53 +0900

    sort by datetime

--- get_cloudwatch_metrics.py
+++ get_cloudwatch_metrics.py
@@ -10,18 +10,22 @@ pp = pprint.PrettyPrinter(indent=4)
 begining_of_today = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
 
 cw = boto3.client('cloudwatch')
-get_metric_statistics = cw.get_metric_statistics(
-                            Namespace='AWS/ELB',
-                            MetricName='HTTPCode_Backend_5XX',
-                            Dimensions=[
-                                {
-                                    'Name': 'LoadBalancerName',
-                                    'Value': elb_name
-                                }
-                            ],
-                            StartTime=begining_of_today - datetime.timedelta(days=14),
-                            EndTime=begining_of_today,
-                            Period=86400,
-                            Statistics=['Sum'])
+metrics = cw.get_metric_statistics(
+            Namespace='AWS/ELB',
+            MetricName='HTTPCode_Backend_5XX',
+            Dimensions=[
+                {
+                    'Name': 'LoadBalancerName',
+                    'Value': elb_name
+                }
+            ],
+            StartTime=begining_of_today - datetime.timedelta(days=14),
+            EndTime=begining_of_today,
+            Period=86400,
+            Statistics=['Sum'])
 
-pp.pprint(get_metric_statistics)
+data_points = metrics['Datapoints']
+
+sorted_data_points = sorted(data_points, key=lambda x: x['Timestamp'])
+
+pp.pprint(sorted_data_points)
